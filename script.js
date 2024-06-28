692d4e0bb680469736cb40feadb0510762db4007
ER3VEN <rob.gatien@gmail.com>
2024-06-28 12:19:18 +0200

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const quotes = [
     "On est 55 milliards sur Terre et faut que ce genre de truc sa arrive à quelqu'un, et cette personne c'est qui, c'est moi. - Tk78",
     "Es ce que c'est bon pour vous. - Squeezie",
     "La racaille de shanghai. - Eric Judor",
-    "Je suis rapide. - Kylian Mbappé qui imite Flash Macqueen",
+    "Je suis rapide. - Flash Macqueen",
     "La meilleure façon de prédire l'avenir c'est d'aller dans le futur. - Anonyme",
     "Le présent c'est ce qui se passe maintenant. - Anonyme",
     "Tu as mal, arrête. - ThéoBabac",
