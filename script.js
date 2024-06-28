b89f28666d43040c669c841a77d8b0cf691f3c91
ER3VEN <rob.gatien@gmail.com>
2024-06-28 11:18:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const quotes = [
+    "Je cuisinerais toujours ce que l'on me commandera. - Cuistot McDo",
+    "Bonjour, bienvenue chez Sephora. - Vendeuse Sephora",
+    "On est 55 milliards sur Terre et faut que ce genre de truc sa arrive à quelqu'un, et cette personne c'est qui, c'est moi. - Tk78",
+    "Es ce que c'est bon pour vous. - Squeezie",
+    "La racaille de shanghai. - Eric Judor",
+    "Je suis rapide. - Kylian Mbappé qui imite Flash Macqueen",
+    "La meilleure façon de prédire l'avenir c'est d'aller dans le futur. - Anonyme",
+    "Le présent c'est ce qui se passe maintenant. - Anonyme"
+];
+
+document.getElementById('new-quote').addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById('quote').innerText = quotes[randomIndex];
+});
